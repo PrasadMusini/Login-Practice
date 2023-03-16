3c97c1d051d96bcbafe309c42685e0b8f0fcdb32
PrasadMusini <prasadmusini1999@gmail.com>
2023-03-16 16:42:25 +0530

    practice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DemoComponent } from './demo/demo.component';
+import { LoginComponent } from './404/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'404',component:LoginComponent},
+  {path:'demo',component:DemoComponent},
+  {path:'',redirectTo:'/demo',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './404/login.component';
+import { DemoComponent } from './demo/demo.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
